0d430c0f6b85eff06d74683ab3ad2a8b1a03fdae
Gabriel Figueiredo <gabrielcarolino962@gmail.com>
2022-04-15 20:39:39 -0300

    Adding database configurations

---
+++ studentWebAPI/studentWebAPI/Context/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using studentWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace studentWebAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    Id = 1,
+                    Name = "Mariazinha",
+                    Email = "mariasicredi@gmail.com",
+                    Age = 20,
+                },
+                new Student
+                {
+                    Id = 2,
+                    Name = "Angelao",
+                    Email = "angelaoCSGO@gmail.com",
+                    Age = 21,
+                }
+            );
+        }
+    }
+}
---
+++ studentWebAPI/studentWebAPI/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace studentWebAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+    }
+}
