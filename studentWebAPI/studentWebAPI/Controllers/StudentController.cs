a60f40617f17d0a566ce482303570df83c23449d
Gabriel Figueiredo <gabrielcarolino962@gmail.com>
2022-04-16 17:18:58 -0300

    ADDED: New controllers to students.

--- studentWebAPI/studentWebAPI/Controllers/StudentController.cs
+++ studentWebAPI/studentWebAPI/Controllers/StudentController.cs
@@ -24,15 +24,73 @@ namespace studentWebAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<IAsyncEnumerable<Student>>> GetStudents()
         {
-            try
-            {
-                var students = await _studentService.GetStudents();
-                return Ok(students);
-            }
-            catch
+            var students = await _studentService.GetStudents();
+
+            if (students == null)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, "Error getStudents");
             }
+
+            return Ok(students);
+        }
+
+        [HttpGet("StudentsForName")]
+        public async Task<ActionResult<IAsyncEnumerable<Student>>> GetStudentsByName([FromQuery] string name)
+        {
+
+            var students = await _studentService.GetStudentsByName(name);
+
+            if (students == null)
+            {
+                return NotFound($"There isn't students with the {name} name");
+            }
+
+            return Ok(students);
+        }
+
+        [HttpGet("{id:int}", Name = "GetStudent")]
+        public async Task<ActionResult<IAsyncEnumerable<Student>>> GetStudent(int id)
+        {
+            var students = await _studentService.GetStudent(id);
+
+            if (students == null)
+            {
+                return NotFound($"There isn't students with id = {id}");
+            }
+
+            return Ok(students);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Student student)
+        {
+            await _studentService.CreateStudent(student);
+            return CreatedAtRoute(nameof(GetStudent), new { id = student.Id }, student);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Edit(int id, [FromQuery] Student student)
+        {
+            if (student.Id == id)
+            {
+                await _studentService.UpdateStudent(student);
+                return Ok($"Student {student.Name} updated");
+            }
+
+            return BadRequest("error when trying to edit");
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Remove(int id)
+        {
+            var student = await _studentService.GetStudent(id);
+
+            if (student == null)
+            {
+                return NotFound($"{student.Name} student don't found");
+            }
+
+            return Ok($"{student.Name} student removed");
         }
     }
 }
